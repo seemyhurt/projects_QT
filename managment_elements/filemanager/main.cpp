2b3e0aad1ebf41d822a547da8355bcce39f998b4
seemyhurt <125909719+seemyhurt@users.noreply.github.com>
2023-02-20 21:38:45 +0300

    Project

---
+++ managment_elements/filemanager/main.cpp
@@ -0,0 +1,25 @@
+
+#include <QtWidgets>
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    QFileSystemModel model;
+    QSplitter spl(Qt::Horizontal);
+    QTreeView treeView;
+    model.setRootPath(QDir::rootPath());
+    QTreeView* pTreeView = new QTreeView;
+    pTreeView->setModel(&model);
+    QTableView* pTableView = new QTableView;
+    pTableView->setModel(&model);
+    QObject::connect(pTreeView, SIGNAL(clicked(const QModelIndex&)),pTableView, SLOT(setRootIndex(const QModelIndex&)));
+
+    QObject::connect(pTableView,SIGNAL(activated(const QModelIndex&) ),pTreeView, SLOT(setCurrentIndex(const QModelIndex&)));
+    QObject::connect(pTableView,SIGNAL(activated(const QModelIndex&)), pTableView,SLOT(setRootIndex(const QModelIndex&)));
+
+    spl.addWidget(pTreeView);
+    spl.addWidget(pTableView);
+    spl.show();
+    return a.exec();
+}
